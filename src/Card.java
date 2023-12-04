d45c9f415a7e02b8d8ea64bdd587156fbc3c445c
victoriahardingbradley <victoria.hardingbradley@menloschool.org>
2023-12-04 11:56:40 -0800

    I created the new classes, and created the methods for random cards.

--- src/Card.java
+++ src/Card.java
@@ -1,40 +1,91 @@
 import java.lang.Math;
 public class Card
 {
-    String suit;
+    static String suit;
     //ace = 1 or 11, king = 10, queen = 9, and jack = 10
-    int num;
-    public class (String newSuit, int newNum)
+    static String rank;
+    int point;
+    public Card (String newSuit, String newRank, int point)
     {
         suit = newSuit;
-        num = newNum;
+        rank = newRank;
+        if(newRank.equals("J") || newRank.equals("Q") || newRank.equals("K"))
+        {
+            point = 10;
+        }
+        else if(!newRank.equals("A"))
+        {
+            point = Integer.parseInt(newRank);
+        }
     }
 
     public static void setSuit(String theSuit)
     {
-        newSuit = theSuit;
+        suit = theSuit;
     }
 
-    public static void setNum(int theNum)
+    public static void setRank(String theRank)
     {
-        newNum = theNum;
+        rank = theRank;
     }
 
+    public void setPoints(int newPoints)
+    {
+        point = newPoints;
+    }
     public String getSuit()
     {
         return suit;
     }
-    public int getNum()
+    public String getRank()
     {
-        return num;
+        return rank;
+    }
+
+    public int getPoints()
+    {
+        return point;
     }
     public Card newCard()
     {
+        String newRank = "";
+        int newPoints;
         int randSuit = (int) (Math.random() * 4);
         int randNum = (int) (Math.random() * 11);
-        String[] suits = ["Spades", "Clubs", "Diamonds", "Hearts"];
-        return newCard(suits[randSuit], randNum);
+        if (randNum == 10)
+        {
+            int randRank = (int) (Math.random() * 3);
+            if (randRank == 1)
+            {
+                newRank = "J";
+                newPoints = 10;
+
+            }
+            else if (randRank == 2)
+            {
+                newRank = "Q";
+                newPoints = 10;
+            }
+            else if (randRank == 3)
+            {
+                newRank = "K";
+                newPoints = 10;
+            }
+        }
+        else if (randNum == 1 || randNum == 11)
+        {
+            newRank = "A";
+        }
+        else
+        {
+            newRank = randNum + "";
+        }
+        String[] suits = new String[4];
+        suits = ["Clubs", "Diamonds", "Hearts"];
+        return new Card(suits[randSuit], newRank, );
     }
+
+    public
 }
 
 
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,30 @@
 public class Player
 {
-    
+    String name;
+    int points = 0;
+    public Player (String playerName)
+    {
+        name = playerName;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+
+    public void setName(String playerName)
+    {
+        name = playerName;
+    }
+
+    public void addPoint()
+    {
+        points++;
+    }
+
+    public int getPoints()
+    {
+        return points;
+    }
 }
